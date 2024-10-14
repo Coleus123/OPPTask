e035906ec5aadd42037b891322daa1a1282bb89d
Temboeddie <154834037+Temboeddie@users.noreply.github.com>
2024-10-14 20:47:37 +0500

    создал класс который хранит себя коллеции из вопросов и ответов

--- src/main/java/ru/urfu/QuesAns.java
+++ src/main/java/ru/urfu/QuesAns.java
@@ -2,72 +2,72 @@ package ru.urfu;
 
 import java.util.ArrayList;
 /**
-        * Класс QuesAns содержит список вопросов и соответствующих ответов.
-        * Он предоставляет методы для доступа к вопросам и их ответам.
-        */
+ * Класс QuesAns содержит список вопросов и соответствующих ответов.
+ * Он предоставляет методы для доступа к вопросам и их ответам.
+ */
 
 public class QuesAns {
-    private static ArrayList<String> questions;
-    private static ArrayList<String> answers;
+    private  ArrayList<String> questions;
+    private  ArrayList<String> answers;
     /**
      * Конструктор для инициализации списков вопросов и ответов.
      */
 
     public QuesAns() {
 
-        questions=new ArrayList<>();
-        answers=new ArrayList<>();
+        this.questions=new ArrayList<>();
+        this.answers=new ArrayList<>();
     }/**
-     * Метод AddBasicQuestion инициализирует список основных вопросов и соответствующих им ответов..
+     * Метод AddBasicQuestion инициализирует список основных вопросов и соответствующих им ответов.
      */
-public void AddBasicQuestion(){
-        questions.add("Чему равен угол равностороннего треугольника?");
-        answers.add("60");
+    public void AddBasicQuestion(){
+        this.questions.add("Чему равен угол равностороннего треугольника?");
+        this.answers.add("60");
 
-        questions.add("Какая столица Франции?");
-        answers.add("Париж");
+        this.questions.add("Какая столица Франции?");
+        this.answers.add("Париж");
 
-        questions.add("В каком числе столько же букв, сколько и в его названии?");
-        answers.add("100");
+        this.questions.add("В каком числе столько же букв, сколько и в его названии?");
+        this.answers.add("100");
 
-        questions.add("Какова скорость света в вакууме (в м/с)?");
-        answers.add("299792458");
+        this.questions.add("Какова скорость света в вакууме (в м/с)?");
+        this.answers.add("299792458");
 
-        questions.add("Какая самая большая планета в нашей Солнечной системе?");
-        answers.add("Юпитер");
-
-
-       questions.add ("Какое наименьшее простое число?");
-       answers.add("2");
+        this.questions.add("Какая самая большая планета в нашей Солнечной системе?");
+        this.answers.add("Юпитер");
 
 
-        questions.add("Кто изобрел телефон?");
-        answers.add("Александр Грейам Белл");
+        this.questions.add ("Какое наименьшее простое число?");
+        this.answers.add("2");
 
 
-        questions.add("Какой химический символ у золота?");
-        answers.add("Au");
+        this.questions.add("Кто изобрел телефон?");
+        this.answers.add("Александр Грейам Белл");
 
 
-        questions.add("В каком году первый человек высадился на Луне?");
-        answers.add("1969");
+        this.questions.add("Какой химический символ у золота?");
+        this.answers.add("Au");
 
-        questions.add("Какой океан самый большой на Земле?");
-        answers.add("Тихий");
-}/**
+
+        this.questions.add("В каком году первый человек высадился на Луне?");
+        this.answers.add("1969");
+
+        this.questions.add("Какой океан самый большой на Земле?");
+        this.answers.add("Тихий");
+    }/**
      * Добавляет новый вопрос и соответствующий ему ответ в список.
      */ public  void addQuestionAndAnswer(String question, String answer) {
-        questions.add(question);
-        answers.add(answer);
+        this.questions.add(question);
+        this.answers.add(answer);
     }
 
     /**
      * Получает вопрос по указанному индексу.
      * @return Строка вопроса.
      */
-    public static String getQuestion(int index) {
-        if (index >= 0 && index < questions.size()) {
-            return questions.get(index);
+    public  String getQuestion(int index) {
+        if (index >= 0 && index < this.questions.size()) {
+            return this.questions.get(index);
         }
         return null;
     }
@@ -77,9 +77,9 @@ public void AddBasicQuestion(){
 
      * @return Строка ответа.
      */
-    public static String getAnswer(int index) {
-        if (index >= 0 && index < answers.size()) {
-            return answers.get(index);
+    public  String getAnswer(int index) {
+        if (index >= 0 && index < this.answers.size()) {
+            return this.answers.get(index);
         }
         return null;
     }
@@ -89,10 +89,21 @@ public void AddBasicQuestion(){
      *
      * @return Количество вопросов.
      */
-    public static int getNumberOfQuestions() {
-        return questions.size();
+    public  int getNumberOfQuestions() {
+        return this.questions.size();
     }
-
+    /**
+     * Удаляет вопрос и соответствующий ответ по указанному индексу.
+     *
+     */
+    public boolean removeQues_and_Ans(int index){
+        if (index >=0 && index<this.questions.size()){
+            questions.remove(index);
+            answers.remove(index);
+            return true;
+        }
+        return false;
+    }
 
 
 }
